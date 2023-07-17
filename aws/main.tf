227df0d78f21dccd8bde4459c3b503db85f2bf37
Roman <iamromandev@gmail.com>
2023-07-17 20:35:13 +0600

    basic test done

--- aws/main.tf
+++ aws/main.tf
@@ -32,7 +32,8 @@ resource "aws_vpc" "vpc" {
 
 # public subnet
 resource "aws_subnet" "public" {
-  count                   = length(var.public_subnet_cidrs)
+  count = length(var.public_subnet_cidrs)
+
   vpc_id                  = aws_vpc.vpc.id
   cidr_block              = element(var.public_subnet_cidrs, count.index)
   availability_zone       = element(local.zones, count.index)
@@ -46,7 +47,8 @@ resource "aws_subnet" "public" {
 
 # private subnet
 resource "aws_subnet" "private" {
-  count                   = length(var.private_subnet_cidrs)
+  count = length(var.private_subnet_cidrs)
+
   vpc_id                  = aws_vpc.vpc.id
   cidr_block              = element(var.private_subnet_cidrs, count.index)
   availability_zone       = element(local.zones, count.index)
@@ -69,14 +71,16 @@ resource "aws_internet_gateway" "ig" {
 
 # elastic ip for nat
 /*resource "aws_eip" "nat_eip" {
-  count      = length(local.zones)
+  count = length(local.zones)
+
   domain     = "vpc"
   depends_on = [aws_internet_gateway.ig]
 }*/
 
 # nat gateway
 /*resource "aws_nat_gateway" "ng" {
-  count         = length(local.zones)
+  count = length(local.zones)
+
   allocation_id = element(aws_eip.nat_eip.*.id, count.index)
   subnet_id     = element(aws_subnet.public.*.id, count.index)
   tags          = {
@@ -95,13 +99,40 @@ resource "aws_route_table" "public" {
 }
 
 # private route table
-resource "aws_route_table" "private" {
+/*resource "aws_route_table" "private" {
+  count = length(aws_nat_gateway.ng)
+
   vpc_id = aws_vpc.vpc.id
   tags   = {
     Environment = var.environment
     Name        = "${var.project}-private-route-table-${var.environment}"
   }
+}*/
+
+# route for internet gateway
+resource "aws_route" "public" {
+  route_table_id         = aws_route_table.public.id
+  gateway_id             = aws_internet_gateway.ig.id
+  destination_cidr_block = "0.0.0.0/0"
 }
 
+# route for nat gateway
+/*resource "aws_route" "private" {
+  count = length(aws_route_table.private)
+
+  route_table_id         = element(aws_route_table.private.*.id, count.index)
+  gateway_id             = element(aws_nat_gateway.ng.*.id, count.index)
+  destination_cidr_block = "0.0.0.0/0"
+}*/
+
+# route table association for public subnets
+resource "aws_route_table_association" "public" {
+  count          = length(var.public_subnet_cidrs)
+  route_table_id = aws_route_table.public.id
+  subnet_id      = element(aws_subnet.public.*.id, count.index)
+}
+
+# route table association for private subnets
+# TODO
 
 
