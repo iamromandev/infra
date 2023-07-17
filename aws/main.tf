9fc1761d5a77d430bcefd89f3c6f78b23e63b231
Roman <iamromandev@gmail.com>
2023-07-17 19:36:07 +0600

    add tfvars, define subnet,  internet gateway, nat gateway

--- aws/main.tf
+++ aws/main.tf
@@ -9,7 +9,13 @@ terraform {
 }
 
 provider "aws" {
-  region = var.aws_region
+  region     = var.aws_region
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+}
+
+locals {
+  zones = ["${var.aws_region}a", "${var.aws_region}b"]
 }
 
 # vpc
@@ -19,9 +25,83 @@ resource "aws_vpc" "vpc" {
   enable_dns_support   = true
 
   tags = {
-    Name        = "${var.environment}-vpc"
     Environment = var.environment
+    Name        = "${var.project}-vpc-${var.environment}"
   }
 }
 
 # public subnet
+resource "aws_subnet" "public" {
+  count                   = length(var.public_subnet_cidrs)
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = element(var.public_subnet_cidrs, count.index)
+  availability_zone       = element(local.zones, count.index)
+  map_public_ip_on_launch = true
+
+  tags = {
+    Environment = var.environment
+    Name        = "${var.project}-public-subnet-${element(local.zones, count.index)}-${var.environment}"
+  }
+}
+
+# private subnet
+resource "aws_subnet" "private" {
+  count                   = length(var.private_subnet_cidrs)
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = element(var.private_subnet_cidrs, count.index)
+  availability_zone       = element(local.zones, count.index)
+  map_public_ip_on_launch = false
+
+  tags = {
+    Environment = var.environment
+    Name        = "${var.project}-private-subnet-${element(local.zones, count.index)}-${var.environment}"
+  }
+}
+
+# internet gateway
+resource "aws_internet_gateway" "ig" {
+  vpc_id = aws_vpc.vpc.id
+  tags   = {
+    Environment = var.environment
+    Name        = "${var.project}-ig-${var.environment}"
+  }
+}
+
+# elastic ip for nat
+/*resource "aws_eip" "nat_eip" {
+  count      = length(local.zones)
+  domain     = "vpc"
+  depends_on = [aws_internet_gateway.ig]
+}*/
+
+# nat gateway
+/*resource "aws_nat_gateway" "ng" {
+  count         = length(local.zones)
+  allocation_id = element(aws_eip.nat_eip.*.id, count.index)
+  subnet_id     = element(aws_subnet.public.*.id, count.index)
+  tags          = {
+    Environment = var.environment
+    Name        = "${var.project}-ng-${element(local.zones, count.index)}-${var.environment}"
+  }
+}*/
+
+# public route table
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.vpc.id
+  tags   = {
+    Environment = var.environment
+    Name        = "${var.project}-public-route-table-${var.environment}"
+  }
+}
+
+# private route table
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.vpc.id
+  tags   = {
+    Environment = var.environment
+    Name        = "${var.project}-private-route-table-${var.environment}"
+  }
+}
+
+
+
--- aws/vars.tf
+++ aws/vars.tf
@@ -1,24 +1,36 @@
+variable "project" {
+  type = string
+}
+
 variable "environment" {
-  default = "dev"
+  type = string
 }
 
 variable "aws_region" {
-  default = "us-east-1"
+  type = string
+}
+
+variable "aws_access_key" {
+  type = string
+}
+
+variable "aws_secret_key" {
+  type = string
 }
 
 variable "vpc_cidr" {
   description = "CIDR block of the vpc"
-  default = "10.0.0.0/16"
+  default     = "10.0.0.0/16"
 }
 
 variable "public_subnet_cidrs" {
-  type = list(any)
-  default = ["10.0.0.0/20", "10.0.128.0/20"]
+  type        = list(string)
+  default     = ["10.0.0.0/20", "10.0.128.0/20"]
   description = "CIDR block for Public Subnet"
 }
 
 variable "private_subnet_cidrs" {
-  type = list(any)
-  default = ["10.0.16.0/20", "10.0.144.0/20"]
+  type        = list(string)
+  default     = ["10.0.16.0/20", "10.0.144.0/20"]
   description = "CIDR block for Private Subnet"
 }
